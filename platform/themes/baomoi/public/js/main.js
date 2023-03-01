78122b505673ca2ec2b68aee79be8730a630ce32
haole1996 <haole042@gmail.com>
2023-03-01 20:14:51 +0700

    cs

--- platform/themes/baomoi/public/js/main.js
+++ platform/themes/baomoi/public/js/main.js
@@ -8,53 +8,5 @@ $(document).ready(function() {
         }
     });
 
-    $("#news-slider").owlCarousel({
-        items : 2,
 
-        itemsDesktop:[1199,3],
-        itemsDesktopSmall:[980,2],
-        itemsMobile : [600,1],
-        navigation:true,
-        navigationText:["",""],
-        pagination:true,
-        autoPlay:true,
-        loop:false,
-        margin:5,
-        responsiveClass:true,
-        stagePadding : 80
-    });
-});
-$(document).ready(function() {
-    $("#news-animal").owlCarousel({
-        items : 2,
-
-        itemsDesktop:[1199,3],
-        itemsDesktopSmall:[980,2],
-        itemsMobile : [600,1],
-        navigation:true,
-        navigationText:["",""],
-        pagination:true,
-        autoPlay:true,
-        loop:false,
-        margin:5,
-        responsiveClass:true,
-        stagePadding : 80
-    });
-});
-$(document).ready(function() {
-    $("#news-cosmetics").owlCarousel({
-        items : 2,
-
-        itemsDesktop:[1199,3],
-        itemsDesktopSmall:[980,2],
-        itemsMobile : [600,1],
-        navigation:true,
-        navigationText:["",""],
-        pagination:true,
-        autoPlay:true,
-        loop:false,
-        margin:5,
-        responsiveClass:true,
-        stagePadding : 80
-    });
 });
